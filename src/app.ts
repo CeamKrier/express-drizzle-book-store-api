fc7810fa6a2efaf75efdb87fa4886c808c064e3c
ceamkrier <ceamkrier@gmail.com>
2024-12-08 01:38:35 +0300

    fallback port set to 3000

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import UsersRouter from '@/routes/users';
 import BooksRouter from '@/routes/books';
 
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ type: 'application/json', limit: '1mb' }));
 
