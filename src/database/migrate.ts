f0a818cb04cb3c87a6ba4adfc47c3f95c2608222
ceamkrier <ceamkrier@gmail.com>
2024-12-08 01:39:38 +0300

    code format related updates

--- src/database/migrate.ts
+++ src/database/migrate.ts
@@ -1,14 +1,14 @@
-import "dotenv/config";
+import 'dotenv/config';
 
-import { drizzle } from "drizzle-orm/node-postgres";
-import { migrate } from "drizzle-orm/node-postgres/migrator";
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { migrate } from 'drizzle-orm/node-postgres/migrator';
 
 const db = drizzle(process.env.PG_DB_URL as string);
 
 async function main() {
-    await migrate(db, {
-        migrationsFolder: "./src/database/migrations"
-    });
+  await migrate(db, {
+    migrationsFolder: './src/database/migrations',
+  });
 }
 
 main().catch(console.error);
--- src/database/schema/index.ts
+++ src/database/schema/index.ts
@@ -1,29 +1,29 @@
-import { pgTable, serial, varchar, timestamp, integer, decimal } from "drizzle-orm/pg-core";
+import { pgTable, serial, varchar, timestamp, integer, decimal } from 'drizzle-orm/pg-core';
 
-export const users = pgTable("users", {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 255 }).notNull(),
-    createdAt: timestamp("created_at").defaultNow().notNull()
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 255 }).notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
 });
 
-export const books = pgTable("books", {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 255 }).notNull(),
-    createdAt: timestamp("created_at").defaultNow().notNull()
+export const books = pgTable('books', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 255 }).notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
 });
 
-export const borrows = pgTable("borrows", {
-    id: serial("id").primaryKey(),
-    userId: integer("user_id")
-        .notNull()
-        .references(() => users.id),
-    bookId: integer("book_id")
-        .notNull()
-        .references(() => books.id),
-    borrowedAt: timestamp("borrowed_at").defaultNow().notNull(),
-    returnedAt: timestamp("returned_at"),
-    score: decimal("score", { precision: 3, scale: 1 }),
-    createdAt: timestamp("created_at").defaultNow().notNull()
+export const borrows = pgTable('borrows', {
+  id: serial('id').primaryKey(),
+  userId: integer('user_id')
+    .notNull()
+    .references(() => users.id),
+  bookId: integer('book_id')
+    .notNull()
+    .references(() => books.id),
+  borrowedAt: timestamp('borrowed_at').defaultNow().notNull(),
+  returnedAt: timestamp('returned_at'),
+  score: decimal('score', { precision: 3, scale: 1 }),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
 });
 
 export type User = typeof users.$inferSelect;
